5d203827daf713e8c1fa0f6c3354527bd9b4a5dc
Zhongheng Liu <77832431+zhongheng-liu@users.noreply.github.com>
2023-10-11 14:28:23 +0300

    add questions.py to worksheet

---
+++ worksheet/questions.py
@@ -0,0 +1,36 @@
+def q1():
+    width = float(input("width: "))
+    length = float(input("length: "))
+    print(f"The surface area is {width * length}")
+def q2():
+    degFah = float(input("Temperature in Fahrenheit: "))
+    degCel = (degFah - 32) * 5 / 9
+    print(f"The converted temperature in degrees celsius is {round(degCel, 1)}") 
+def q3():
+    TempDegCel = float(input("Temperature in degrees Celsius: "))
+    if TempDegCel > 0: print("Temperature is not freezing")
+    else: print("Temperature is freezing")
+def q4():
+    TempDegCel = float(input("Temperature of water in degrees Celsius: "))
+    if TempDegCel < 0: print("Water is frozen")
+    elif TempDegCel >= 100: print("Water is boiling")
+    else: 
+        print("Water is liquid")
+    return
+def q5():
+    months = [
+        "January", "February", "March", "April", "May", "June", "July", "August",
+        "September", "October", "November", "December"
+    ]
+    index = int(input("Index of month: "))
+    if index > 12 or index < 1: print("Invalid")
+    print(months[index - 1])
+    return
+def q6():
+    return
+q1()
+q2()
+q3()
+q4()
+q5()
+q6()
