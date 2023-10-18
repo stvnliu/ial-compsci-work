3508b10bd117ce0c09a73b4942188dbcb94a2ec3
Zhongheng Liu <77832431+zhongheng-liu@users.noreply.github.com>
2023-10-18 11:23:02 +0000

    add q6 q7 to worksheet

--- worksheet/questions.py
+++ worksheet/questions.py
@@ -7,8 +7,8 @@ def q2():
     degCel = (degFah - 32) * 5 / 9
     print(f"The converted temperature in degrees celsius is {round(degCel, 1)}") 
 def q3():
-    TempDegCel = float(input("Temperature in degrees Celsius: "))
-    if TempDegCel > 0: print("Temperature is not freezing")
+    tempDegCel = float(input("Temperature in degrees Celsius: "))
+    if tempDegCel > 0: print("Temperature is not freezing")
     else: print("Temperature is freezing")
 def q4():
     TempDegCel = float(input("Temperature of water in degrees Celsius: "))
@@ -27,10 +27,29 @@ def q5():
     print(months[index - 1])
     return
 def q6():
+    num = int(input("Input positive integer: "))
+    while num <= 0:
+        num = int(input("Please re-enter the POSITIVE INTEGER: "))
+    output = ""
+    for i in range(num):
+        output += str(i+1) + " "
+    print(output)
     return
-q1()
-q2()
-q3()
-q4()
-q5()
+def q7():
+    num_end = int(input("Input positive integer as ending: "))
+    while num_end <= 0:
+        num_end = int(input("Please re-enter the POSITIVE INTEGER: "))
+    stack = []
+    output = ""
+    for num in range(num_end):
+        stack.append(num)
+    for _ in range(len(stack)):
+        output += str(stack.pop()+1) + " "
+    print(output)
+# q1()
+# q2()
+# q3()
+# q4()
+# q5()
 q6()
+q7()
