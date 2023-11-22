ebbe39c805001d2e709cd25c02f103f2d6758d45
Zhongheng Liu <z.liu@outlook.com.gr>
2023-11-22 17:26:25 +0200

    act. 10a 10b 10c

---
+++ activities/ch10/10c.q3.ts
@@ -0,0 +1,12 @@
+type StudentRecord {
+	name: string,
+	dateOfBirth: Date,
+	class: string,
+	gender: string
+}
+let myStudent: StudentRecord = {
+	name: "Ahmad Sayed",
+	dateOfBirth: "2023-03-10",
+	class: "5A",
+	gender: "Male"
+}
