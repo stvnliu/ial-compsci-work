a0c42404c84698be97b9208cdfa5d53677dbc440
Zhongheng Liu <z.liu@outlook.com.gr>
2023-11-24 10:59:00 +0200

    python impl for act. 10e

---
+++ activities/ch10/10e.py
@@ -0,0 +1,12 @@
+myArray: list = []
+# Population
+for row_ptr in range(2):
+    row = []
+    for col_ptr in range(3):
+        row.append(input(f"Value at pos ({row_ptr}, {col_ptr}): "))
+    myArray.append(row)
+print(myArray)
+output = ""
+for row in myArray:
+    output += " ".join(row) + "\n"
+print(output)
