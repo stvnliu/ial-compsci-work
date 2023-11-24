24523e822eb358200b917d4fb650aa6dfa29b1b5
Justin He <acgentry060814@gmail.com>
2023-11-24 11:23:25 +0200

    Update 10e.py
    999999999999999999999999

--- activities/ch10/10e.py
+++ activities/ch10/10e.py
@@ -2,7 +2,7 @@ myArray: list = []
 # Population
 for row_ptr in range(2):
     row = []
-    for col_ptr in range(3):
+    for col_ptr in range(9):
         row.append(input(f"Value at pos ({row_ptr}, {col_ptr}): "))
     myArray.append(row)
 print(myArray)
