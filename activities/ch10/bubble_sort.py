bda939b32da2527c2da310fa8cb69cb1764a03d4
Zhongheng Liu <z.liu@outlook.com.gr>
2023-11-29 22:00:14 +0200

    commit

--- activities/ch10/bubble_sort.py
+++ activities/ch10/bubble_sort.py
@@ -1,32 +1,30 @@
-# import random                                 # For testing sequence
+import random                                   # For testing sequence
 def generate_test_seq(length: int):
     arr = []
     for i in range(length):
         arr.append(random.randint(0, 100))
     return arr
-arr = [i for i in range(20)]
-print(arr)
-sortcount = 0
-index = 0
-diff = 0
-numiter = 0
-while True:
-    if sortcount == len(arr) - diff - 1:        # Magic number, don't know why it works but it works
-        break
-    index = 0
-    sortcount = 0
-    while index < len(arr) - diff - 1:
-        if arr[index] < arr[index+1]:           # Not in order, sorting is needed
-            swp = arr[index]
-            arr[index] = arr[index+1]
-            arr[index+1] = swp
-        else:
-            sortcount += 1
-        index += 1
-        numiter += 1
-        # print(arr)                            # for debug
-    diff += 1                                   # At the end of each iteration, we know that the last element in the array is sorted. It is unnecessary to check up to that location in the next iteration.
-
+TESTING_ITERATIONS = 10
+def bubble_sort(arr):
+    n = len(arr)
+    swapped = False
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                swapped = True
+                swp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = swp
+        if not swapped:
+            return
+def verify(arr):
+    for index in range(len(arr)-1):
+        if arr[index] > arr[index+1]: return False
+    return True
 # Output result
-print(arr)
-print(f"Done in {numiter} iterations.")
+if __name__ == "__main__":
+    for i in range(TESTING_ITERATIONS):
+        arr = generate_test_seq(10)
+        bubble_sort(arr)
+        print(arr)
+    print(f"Testing for {TESTING_ITERATIONS} arrays completed.")
