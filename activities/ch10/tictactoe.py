d50fe5bcf2b329b195898d1df24ba7eadf39b959
Zhongheng Liu <z.liu@outlook.com.gr>
2023-11-30 22:01:01 +0200

    tic tac toe

--- activities/ch10/tictactoe.py
+++ activities/ch10/tictactoe.py
@@ -1,11 +1,7 @@
 board = [
-    [
-        " ", " ", " "
-    ], [
-        " ", " ", " "
-    ], [
-        " ", " ", " "
-    ]
+    " ", " ", " ",
+    " ", " ", " ",
+    " ", " ", " "
 ]
 wins = [
     [0, 1, 2],
@@ -13,29 +9,40 @@ wins = [
     [6, 7, 8],
     [0, 4, 8],
     [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
 ]
 def print_board():
-    for row in board:
-        print("|"+ "|".join(row)+"|")
-print_board() 
+    split = 3
+    index = 0
+    output = "|"
+    while index < len(board):
+        output += board[index] + "|"
+        if (index + 1) % 3 == 0:
+            print(output)
+            output = "|"
+        index += 1
+
 def move(move: str, r: int, c: int):
-    board[r][c] = move
+    board[c+3*r] = move
     return
 def test_win(char: str) -> bool:
-    won = True
+    won = False
     for cond in wins:
-        if board[cond[0]] != board[cond[1]] != board[cond[2]]:
-            won = False
+        if (board[cond[0]] == board[cond[1]] == board[cond[2]]) and  \
+            (board[cond[0]] != " " and board[cond[1]] != " " and board[cond[2]] != " "):
+            won = True
     return won
 def game():
     cur = "X"
     cont = True
     while cont:
-        print_board()
-        decision = input(f"Where to put {cur}? : ")
+        decision = input(f"Where to put {cur}? (row column): ")
         [r, c] = decision.split(" ")
-        move(cur, int(r), int(c))
+        move(cur, int(r)-1, int(c)-1)
         won = test_win(cur)
+        print_board()
         if won:
             print(f"{cur} won!")
             return
